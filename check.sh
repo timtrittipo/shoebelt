e4b95bb4282bc0a11f98f6b8d37ef6c47168e1df
Tim <timtrittipo@users.noreply.github.com>
2017-06-22 11:58:23 -0700

    rm tags.sh

--- check.sh
+++ check.sh
@@ -4,24 +4,21 @@ gitHash7=$(git rev-parse --short HEAD || :)                # $gitHash7 is a 7 ch
 gitTagExact=$(git describe --exact-match 2>/dev/null || :) # fatal: no tag exactly matches '010000000000000..'
 gitTag=$(git describe --tags 2>/dev/null || :)             #1.1.2222-123-abcdef4
 
-echo "0 ${#gitHash7}"
-echo "1 ${#gitTagExact}"
-echo "2 ${#gitTag}"
-echo "0 ${gitHash7}"
-echo "1 ${gitTagExact}"
-echo "2 ${gitTag}"
+echo " -- "
+printf "%-9s %-18s %-8s\n" gitHash7 tagExct tag
+printf "%-9s %-18s %-8s\n" ${#gitHash7} ${#gitTagExact} ${#gitTag}
+printf "%-9s %-18s %-8s\n" ${gitHash7} ${gitTagExact:-NA} ${gitTag}
+echo " -- "
 
 if [[ ${gitTag} =~ -[[:digit:]]-[[:alnum:]] ]]; then
   echo "commit after tag"
-elif [[ ${gitTag} =~ ^[[:digit:]]+\.[[:digit:]]+\.[[:alpha:]]\.[[:digit:]]\.[[:digit:]]$ ]]; then
+elif [[ ${gitTag} =~ ^[[:digit:]]+\.[[:digit:]]+\.[[:alpha:]]+\.[[:digit:]]+\.[[:digit:]]+$ ]]; then
   echo "good tag"
-  # 3.0.A.5.4, so that the regex [[:digit:]]+.[[:digit:]]+.[[:alpha:]]
 fi
 
-gitTag="${1}"
-if [[ ${gitTag} =~ -[[:digit:]]-[[:alnum:]] ]]; then
-  echo "commit after tag"
+if [[ "$1" ]]; then
+  gitTag="${1}"
+  [[ ${gitTag} =~ -[[:digit:]]-[[:alnum:]] ]] && echo "commit after tag"
+  [[ ${gitTag} =~ ^[[:digit:]]+\.[[:digit:]]+\.[[:alpha:]]+\.[[:digit:]]+\.[[:digit:]]+$ ]] &&  echo "good tag"
+  # [[ ${gitTag} =~ ^([[:digit:]]+\.){2}[[:alpha:]]+\.[[:digit:]]+\.[[:digit:]]+$ ]] &&  echo "good tag regex 2"
 fi
-[[ ${gitTag} =~ ^[[:digit:]]+\.[[:digit:]]+\.[[:alpha:]]+\.[[:digit:]]+\.[[:digit:]]+$ ]] &&  echo "good tag"
-  # 3.0.A.5.4, so that the regex [[:digit:]]+.[[:digit:]]+.[[:alpha:]]
-# [[ ${gitTag} =~ ^([[:digit:]]+\.){2}[[:alpha:]]+\.[[:digit:]]+\.[[:digit:]]+$ ]] &&  echo "good tag regex 2"
